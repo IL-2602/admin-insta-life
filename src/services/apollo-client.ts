c9ad65216e8def5b6413c06a7876cf0abcc28a07
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-06-07 12:36:51 +0300

    IL-143 feat: add configs for graphql

---
+++ src/services/apollo-client.ts
@@ -0,0 +1,43 @@
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_APOLLO_BASE_API_URL,
+})
+
+const wsLink = new WebSocketLink({
+  options: {
+    reconnect: true,
+  },
+  uri: process.env.NEXT_PUBLIC_APOLLO_WS_BASE_API_URL!,
+})
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
+
+const authLink = setContext((_, { headers }) => {
+  const base64password = btoa('admin@gmail.com:admin')
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: `Basic ${base64password}`,
+    },
+  }
+})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(splitLink),
+})
+
+export default client
