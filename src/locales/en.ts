ad5658c9db61827582f0d3269d577617907aa0d8
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-06-10 11:39:10 +0300

    IL-144 feat: add translations and route

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,1 +1,9 @@
-export const en = {}
+export const en = {
+  auth: {
+    error: 'Incorrect email or password',
+    labelEmail: 'Email',
+    labelPassword: 'Password',
+    submit: 'Sign in',
+    title: 'Sign in',
+  },
+}
--- src/locales/ru.ts
+++ src/locales/ru.ts
@@ -1,1 +1,9 @@
-export const ru = {}
+export const ru = {
+  auth: {
+    error: 'Неверные почта или пароль',
+    labelEmail: 'Почта',
+    labelPassword: 'Пароль',
+    submit: 'Войти',
+    title: 'Войти',
+  },
+}
