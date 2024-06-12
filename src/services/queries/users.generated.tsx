754b8dea1f030579669f553905ebe14c568cd6c5
alexsharich <alexsharich@gmail.com>
2024-06-12 22:43:48 +0300

    IL-157 feat: add ban/unban user endpoints

--- src/services/queries/users.generated.tsx
+++ src/services/queries/users.generated.tsx
@@ -13,6 +13,19 @@ export type GetUserQuery = {
   getUser: { __typename?: 'User'; createdAt: any; email: string; id: number; userName: string }
 }
 
+export type BanUserMutationVariables = Types.Exact<{
+  banReason: Types.Scalars['String']['input']
+  userId: Types.Scalars['Int']['input']
+}>
+
+export type BanUserMutation = { __typename?: 'Mutation'; banUser: boolean }
+
+export type UnbanUserMutationVariables = Types.Exact<{
+  userId: Types.Scalars['Int']['input']
+}>
+
+export type UnbanUserMutation = { __typename?: 'Mutation'; unbanUser: boolean }
+
 export type GetUsersQueryVariables = Types.Exact<{
   pageNumber?: Types.InputMaybe<Types.Scalars['Int']['input']>
   pageSize?: Types.InputMaybe<Types.Scalars['Int']['input']>
@@ -98,6 +111,87 @@ export type GetUserQueryHookResult = ReturnType<typeof useGetUserQuery>
 export type GetUserLazyQueryHookResult = ReturnType<typeof useGetUserLazyQuery>
 export type GetUserSuspenseQueryHookResult = ReturnType<typeof useGetUserSuspenseQuery>
 export type GetUserQueryResult = Apollo.QueryResult<GetUserQuery, GetUserQueryVariables>
+export const BanUserDocument = gql`
+  mutation BanUser($banReason: String!, $userId: Int!) {
+    banUser(banReason: $banReason, userId: $userId)
+  }
+`
+export type BanUserMutationFn = Apollo.MutationFunction<BanUserMutation, BanUserMutationVariables>
+
+/**
+ * __useBanUserMutation__
+ *
+ * To run a mutation, you first call `useBanUserMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useBanUserMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [banUserMutation, { data, loading, error }] = useBanUserMutation({
+ *   variables: {
+ *      banReason: // value for 'banReason'
+ *      userId: // value for 'userId'
+ *   },
+ * });
+ */
+export function useBanUserMutation(
+  baseOptions?: Apollo.MutationHookOptions<BanUserMutation, BanUserMutationVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+
+  return Apollo.useMutation<BanUserMutation, BanUserMutationVariables>(BanUserDocument, options)
+}
+export type BanUserMutationHookResult = ReturnType<typeof useBanUserMutation>
+export type BanUserMutationResult = Apollo.MutationResult<BanUserMutation>
+export type BanUserMutationOptions = Apollo.BaseMutationOptions<
+  BanUserMutation,
+  BanUserMutationVariables
+>
+export const UnbanUserDocument = gql`
+  mutation UnbanUser($userId: Int!) {
+    unbanUser(userId: $userId)
+  }
+`
+export type UnbanUserMutationFn = Apollo.MutationFunction<
+  UnbanUserMutation,
+  UnbanUserMutationVariables
+>
+
+/**
+ * __useUnbanUserMutation__
+ *
+ * To run a mutation, you first call `useUnbanUserMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useUnbanUserMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [unbanUserMutation, { data, loading, error }] = useUnbanUserMutation({
+ *   variables: {
+ *      userId: // value for 'userId'
+ *   },
+ * });
+ */
+export function useUnbanUserMutation(
+  baseOptions?: Apollo.MutationHookOptions<UnbanUserMutation, UnbanUserMutationVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+
+  return Apollo.useMutation<UnbanUserMutation, UnbanUserMutationVariables>(
+    UnbanUserDocument,
+    options
+  )
+}
+export type UnbanUserMutationHookResult = ReturnType<typeof useUnbanUserMutation>
+export type UnbanUserMutationResult = Apollo.MutationResult<UnbanUserMutation>
+export type UnbanUserMutationOptions = Apollo.BaseMutationOptions<
+  UnbanUserMutation,
+  UnbanUserMutationVariables
+>
 export const GetUsersDocument = gql`
   query GetUsers(
     $pageSize: Int
@@ -138,6 +232,7 @@ export const GetUsersDocument = gql`
     }
   }
 `
+
 /**
  * __useGetUsersQuery__
  *
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -10,6 +10,17 @@ export const GET_USER = gql`
     }
   }
 `
+export const BAN_USER = gql`
+  mutation BanUser($banReason: String!, $userId: Int!) {
+    banUser(banReason: $banReason, userId: $userId)
+  }
+`
+
+export const UNBAN_USER = gql`
+  mutation UnbanUser($userId: Int!) {
+    unbanUser(userId: $userId)
+  }
+`
 
 export const GET_USERS = gql`
   query GetUsers(
