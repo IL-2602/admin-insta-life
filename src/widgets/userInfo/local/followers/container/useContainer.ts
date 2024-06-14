5c0a437d43c31f35fe742dff3a6efddb577558b3
Papperla <krivman@Savar.local>
2024-06-14 16:53:11 +0200

    IL-155 feat: add state currentPage,currentSize, add use router for userId from ulr

--- src/widgets/userInfo/local/followers/container/useContainer.ts
+++ src/widgets/userInfo/local/followers/container/useContainer.ts
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { GET_FOLLOWERS } from '@/services/queries/followers'
 import {
   GetFollowersQuery,
@@ -5,23 +7,39 @@ import {
 } from '@/services/queries/followers.generated'
 import { SortDirection } from '@/services/types'
 import { useTranslation } from '@/shared/hooks/useTranslation'
+import { HeadCellSort } from '@/shared/ui/Table/Table'
 import getFromLocalStorage from '@/shared/utils/localStorage/getFromLocalStorage'
+import {
+  INITIAL_PAGE_NUMBER,
+  INITIAL_PAGE_SIZE,
+} from '@/widgets/usersList/publ/usersList/constants/constants'
 import { useQuery } from '@apollo/client'
+import { useRouter } from 'next/router'
 
 export const useContainer = () => {
   const base64password = getFromLocalStorage('base64credentials', '')
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [currentSize, setCurrentSize] = useState<number>(10)
+  const [sort, setSort] = useState<HeadCellSort>({ direction: 'Asc', key: 'createdAt' })
+
+  const { query } = useRouter()
+
+  const uId = +(query?.id || '')
+
   const { data, loading } = useQuery<GetFollowersQuery, GetFollowersQueryVariables>(GET_FOLLOWERS, {
     context: { base64password },
     variables: {
-      pageNumber: 1,
-      pageSize: 10,
-      sortBy: 'userName',
-      sortDirection: SortDirection.Asc,
-      userId: 3,
+      pageNumber: currentPage,
+      pageSize: currentSize,
+      sortBy: sort.key,
+      sortDirection: SortDirection[sort.direction],
+      userId: uId,
     },
   })
   const { t } = useTranslation()
   const followers = data?.getFollowers.items
+  const handleSortTable = (sort: HeadCellSort | null) =>
+    sort ? setSort(sort) : setSort({ direction: 'Desc', key: 'createdAt' })
 
-  return { followers, loading, t }
+  return { followers, handleSortTable, loading, sort, t }
 }
--- src/widgets/userInfo/local/followers/ui/Followers.tsx
+++ src/widgets/userInfo/local/followers/ui/Followers.tsx
@@ -8,56 +8,62 @@ import Link from 'next/link'
 
 import s from './Followers.module.scss'
 
-export const Followers = memo(({ followers, loading, t }: FollowersProps) => {
-  if (loading) {
-    return <div>Loading</div>
+export const Followers = memo(
+  ({ followers, handleSortTable, loading, sort, t }: FollowersProps) => {
+    if (loading) {
+      return <div>Loading</div>
+    }
+
+    return (
+      <Table.Root className={s.container}>
+        <Table.Head>
+          <Table.Row>
+            <Table.HeadCell>{t.followers.uId}</Table.HeadCell>
+            <Table.HeadCell name={'userName'} onSort={handleSortTable} sort={sort}>
+              {t.followers.uName}
+            </Table.HeadCell>
+            <Table.HeadCell>{t.followers.link}</Table.HeadCell>
+            <Table.HeadCell name={'atCreated'} onSort={handleSortTable} sort={sort}>
+              {t.followers.followDate}
+            </Table.HeadCell>
+          </Table.Row>
+        </Table.Head>
+
+        <Table.Body>
+          {followers?.length === 0 && (
+            <Table.Row>
+              <Table.Cell colSpan={4}>
+                <Typography className={s.searchNotFound} color={'form'}>
+                  {t.usersList.searchNotFound}
+                </Typography>
+              </Table.Cell>
+            </Table.Row>
+          )}
+
+          {loading ? (
+            <Table.Row>
+              <Table.Cell colSpan={4}>
+                <div className={s.spinner}>
+                  <Spinner />
+                </div>
+              </Table.Cell>
+            </Table.Row>
+          ) : (
+            followers?.map(user => {
+              return (
+                <Table.Row key={user.id}>
+                  <Table.Cell>{user.id}</Table.Cell>
+                  <Table.Cell>{user.userName}</Table.Cell>
+                  <Table.Cell>
+                    <Link className={s.link} href={'#'}>{`>>>O,O<<<`}</Link>
+                  </Table.Cell>
+                  <Table.Cell>{new Date(user.createdAt).toLocaleDateString()}</Table.Cell>
+                </Table.Row>
+              )
+            })
+          )}
+        </Table.Body>
+      </Table.Root>
+    )
   }
-
-  return (
-    <Table.Root className={s.container}>
-      <Table.Head>
-        <Table.Row>
-          <Table.HeadCell>{t.followers.uId}</Table.HeadCell>
-          <Table.HeadCell>{t.followers.uName}</Table.HeadCell>
-          <Table.HeadCell>{t.followers.link}</Table.HeadCell>
-          <Table.HeadCell>{t.followers.followDate}</Table.HeadCell>
-        </Table.Row>
-      </Table.Head>
-
-      <Table.Body>
-        {followers?.length === 0 && (
-          <Table.Row>
-            <Table.Cell colSpan={4}>
-              <Typography className={s.searchNotFound} color={'form'}>
-                {t.usersList.searchNotFound}
-              </Typography>
-            </Table.Cell>
-          </Table.Row>
-        )}
-
-        {loading ? (
-          <Table.Row>
-            <Table.Cell colSpan={4}>
-              <div className={s.spinner}>
-                <Spinner />
-              </div>
-            </Table.Cell>
-          </Table.Row>
-        ) : (
-          followers?.map(user => {
-            return (
-              <Table.Row key={user.id}>
-                <Table.Cell>{user.id}</Table.Cell>
-                <Table.Cell>{user.userName}</Table.Cell>
-                <Table.Cell>
-                  <Link className={s.link} href={'#'}>{`>>>O,O<<<`}</Link>
-                </Table.Cell>
-                <Table.Cell>{new Date(user.createdAt).toLocaleDateString()}</Table.Cell>
-              </Table.Row>
-            )
-          })
-        )}
-      </Table.Body>
-    </Table.Root>
-  )
-})
+)
